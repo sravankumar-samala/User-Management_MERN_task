42116d7c7c071c4d451ef41a6ad64114e28db699
sravankumar-samala <sravankumarsamala20@gmail.com>
2024-06-06 16:01:35 +0530

    Updated not found component

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -1,3 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 export default function NotFound() {
-  return <h1>Not Found</h1>;
+  const navigate = useNavigate();
+  return (
+    <div className="not-found-container">
+      <h1>404 Not Found</h1>
+      <p>The page your are looking for is not found.</p>
+      <button type="button" onClick={() => navigate("/")}>
+        Home
+      </button>
+    </div>
+  );
 }
